d458ca8870a4926dc997a8c7c8251c2f1a3309cc
github username <jeffreyhwang90@gmail.com>
2018-04-29 23:25:40 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[self.length-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[self.length-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[self.length-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    array = self.split
+    count = 0
+      for fragment in array
+        if fragment.sentence? || fragment.question? || fragment.exclamation?
+          count += 1
+        end
+      end
+    count
   end
 end
